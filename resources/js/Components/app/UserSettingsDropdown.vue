1e25ae3725b7d3daab79601e89e8724a25fce6ea
Your Name <youremail@example.com>
2023-09-02 15:34:08 +0700

    dashboard vue components

---
+++ resources/js/Components/app/UserSettingsDropdown.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        User Settings
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,18 +1,19 @@
 <script setup>
-import Navigation from '@/Components/Navigation.vue';
-import SearchForm from '@/Components/SearchForm.vue';
+import Navigation from '@/Components/app/Navigation.vue';
+import SearchForm from '@/Components/app/SearchForm.vue';
+import UserSettingsDropdown from '@/Components/app/UserSettingsDropdown.vue'
 </script>
 
 <template>
     <div class="h-screen bg-gray-70 flex w-full gap-4">
         <Navigation />      
-    </div>
+    
 
     <main class="flex flex-col flex-1 px-4 overflow-hidden">
         <div class="flex items-center justify-between w-full">
             <SearchForm />
-
+            <UserSettingsDropdown />
         </div>
-
     </main>
+</div>
 </template>
