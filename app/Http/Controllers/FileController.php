d44135a0db496b46a181fe7e543a79f3d6adae6f
Your Name <youremail@example.com>
2023-09-03 19:18:28 +0700

    foler & file Request class rules

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class FileController extends Controller
 {
-    //
+    public function myFiles()
+    {
+        return Inertia::render('MyFiles');
+    }
+    
+    public function createFolder()
+    {
+        
+    }
 }
--- app/Models/File.php
+++ app/Models/File.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use App\Traits\HasCreatorAndUpdater;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 
 class File extends Model
 {
-    use HasFactory;
+    use HasFactory, HasCreatorAndUpdater, NodeTrait, SoftDeletes;
+
+    public function isOwnedBy($userId): bool
+    {  
+        return $this->created_by == $userId;
+
+    }
 }
