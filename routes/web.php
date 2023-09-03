7819397e5e8b641522246e1c70ab9dde21759311
Your Name <youremail@example.com>
2023-09-03 19:25:15 +0700

    Web Routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Inertia\Inertia;
 |
 */
 
+// Default route for the home page
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -25,21 +26,30 @@ Route::get('/', function () {
     ]);
 });
 
+// Dashboard route, accessible only to authenticated and verified users
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// FileController routes grouped with authentication and verification middleware
 Route::controller(\App\Http\Controllers\FileController::class)
     ->middleware(['auth', 'verified'])
     ->group(function () {
+        // Route for displaying user's files
         Route::get('/my-files', 'myFiles')->name('myFiles');
+        // Route for creating a folder
         Route::get('/folder/create', 'creatFolder')->name('folder.create');
     });
 
+// ProfileController routes grouped with authentication middleware
 Route::middleware('auth')->group(function () {
+    // Route for editing user's profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route for updating user's profile
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route for deleting user's profile
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Include authentication-related routes
 require __DIR__.'/auth.php';
