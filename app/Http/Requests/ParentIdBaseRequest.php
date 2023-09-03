cdc21f7502dcfd09ac81f53fdd191ed6f6ee1664
Your Name <youremail@example.com>
2023-09-03 19:21:38 +0700

    ParentBaseRequest

--- app/Http/Requests/ParentIdBaseRequest.php
+++ app/Http/Requests/ParentIdBaseRequest.php
@@ -17,11 +17,15 @@ class ParentIdBaseRequest extends FormRequest
      */
     public function authorize(): bool
     {
+        // Attempt to find a File record with the 'id' provided in 'parent_id'.
         $this->parent = File::query()->where('id', $this->input('parent_id'))->first();
 
+        // If a parent File is found and it's not owned by the authenticated user, deny access.
         if ($this->parent && !$this->parent->isOwnedBy(Auth::id())) {
             return false;
         }
+        
+        // If the parent is either not found or is owned by the authenticated user, allow access.
         return true;
     }
 
@@ -34,13 +38,14 @@ class ParentIdBaseRequest extends FormRequest
     {
         return [
             'parent_id' => [
+                // Rule to check if 'parent_id' exists in the 'File' model's 'id' column.
                 Rule::exists(File::class, 'id')
-                ->where(function(Builder $query) {
-                    return $query
-                    ->where('is_folder', '=', '1')
-                    ->where('created_by', '=', Auth::id());
-
-                })
+                    ->where(function(Builder $query) {
+                        // Additional conditions for the existence rule:
+                        return $query
+                            ->where('is_folder', '=', '1') // Check if it's a folder.
+                            ->where('created_by', '=', Auth::id()); // Check if created by the authenticated user.
+                    })
             ]
         ];
     }
