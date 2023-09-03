ee2c2e939b3df3f704a1fae559658790e6b827b0
Your Name <youremail@example.com>
2023-09-03 19:20:42 +0700

    StorefolderRequest

--- app/Http/Requests/StoreFolderRequest.php
+++ app/Http/Requests/StoreFolderRequest.php
@@ -17,14 +17,18 @@ class StoreFolderRequest extends ParentIdBaseRequest
      */
     public function rules(): array
     {
+        // Merge the parent's validation rules with additional rules for this request.
         return array_merge(parent::rules(),
             [
                 'name' => [
-                    'required',
+                    'required', // 'name' field is required.
+                    
+                    // Rule for ensuring the 'name' field is unique in the 'File' model's 'name' column
                     Rule::unique(File::class, 'name')
-                    ->where('created_by', Auth::id())
-                    ->where('parent_id, $this->parent_id')
-                    ]
+                        ->where('created_by', Auth::id()) // Additional condition: created by the authenticated user.
+                        ->where('parent_id', $this->parent_id) // Additional condition: matching 'parent_id' value.
+                        ->whereNull('deleted_at') // Additional condition: 'deleted_at' column is null (record is not deleted).
+                ]
             ]
         ); 
     }
